b6245622e16c8690080a8d7e4d1ef7ba8943db40
vphong <vphong@ucsd.edu>
2018-07-14 17:23:47 -0700

    reset project: drop react native, migrate to swift/fully native

---
+++ animini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animini
+//
+//  Created by Vanna Phong on 7/14/18.
+//  Copyright © 2018 vphong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
